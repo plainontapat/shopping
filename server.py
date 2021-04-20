3902ee4528b26735f39c348e9e3cdec493e53ee6
RabbltC <6106021610049@fitm.kmutnb.ac.th>
2021-04-20 19:35:01 +0700

    commit

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = "hello"
 
 client = MongoClient(
-    "mongodb://admin:FGCxns24841@node12656-shopping.app.ruk-com.cloud:27017"
+    "mongodb://admin:FGCxns24841@node12656-shopping.app.ruk-com.cloud:11007"
 )
 mydb = client["Shopping"]
 stock = mydb["Stock"]
